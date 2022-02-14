5c547da08307de1e1101948be918b56431a332eb
sebastián velez <sebastianvelezarav@gmail.com>
2022-02-14 12:00:22 +0100

    ejercicio 7 done / sprint finalizado

--- src/films.js
+++ src/films.js
@@ -12,7 +12,6 @@ function getMoviesFromDirector(array, director) {
 
 function moviesAverageOfDirector(array, director) { 
   const moviesDirector = getMoviesFromDirector(array, director); 
-  // console.log('estas son las movies por director ', moviesDirector)
   const scores = moviesDirector.reduce((a, b) => ({score: a.score + b.score})); 
   const average = parseFloat((scores.score / moviesDirector.length).toFixed(2)); 
   return average; 
@@ -24,14 +23,12 @@ function orderAlphabetically(array) {
   return orderedNames.slice(0,20);
 }
 
-// Exercise 5: Order by year, ascending
 function orderByYear(array) {
   const titleAndYear = array.map(film => {return {title: film.title, year: film.year}});
   const orderedList = titleAndYear.sort((a, b) => (a.year > b.year) ? 1 : (a.year === b.year) ? ((a.title > b.title) ? 1 : -1) : -1 );
   return orderedList;
 }
 
-// Exercise 6: Calculate the average of the movies in a category
 function moviesAverageByCategory(array, genre) {
   const moviesCategory = array.filter(movie => movie.genre.includes(genre));
   const moviesCategoryWithScore = moviesCategory.filter(movie => movie.score !== '');
@@ -40,30 +37,27 @@ function moviesAverageByCategory(array, genre) {
   return average;
 }
 
-// Exercise 7: Modify the duration of movies to minutes
 function hoursToMinutes(array) {
-  // const newArray = array.map(film => film);
-  const newArray = Object.assign([], array);
-  for (let movie of newArray){
+  const newArray = array.map(movie => {
+    let total = 0;
     if (movie.duration.includes('h') && movie.duration.includes('min')) {
       let time = movie.duration.split(' ');
       let hours = time[0].slice(0, -1);
       let minutes = time[1].slice(0, -3);
-      movie.duration = (parseInt(hours) * 60) + parseInt(minutes);
+      total = (parseInt(hours) * 60) + parseInt(minutes);
     } else if (movie.duration.includes('h')) {
-      let minutes = movie.duration.slice(0, -1) * 60;
-      movie.duration = minutes;
+      total = parseInt(movie.duration.slice(0, -1)) * 60;
     } else {
-      let minutes = movie.duration.slice(0, -3);
-      movie.duration = minutes;
+      total = parseInt(movie.duration.slice(0, -3));
     }
-  }
-  console.log(newArray)
-  console.log('ESTEES',array)
-  return newArray;
+    return {
+      ...movie,
+      duration: total
+    };
+  });
+  return newArray
 }
 
-// Exercise 8: Get the best film of a year
 function bestFilmOfYear(array, year) {
   const moviesOfYear = array.filter(film => film.year === year);
   const filmsOrdered = moviesOfYear.sort((a, b) => (a.score > b.score) ? 1 : -1);
